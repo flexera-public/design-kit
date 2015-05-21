b7022cc1c00d0188bdde6b2647116dec420d7d04
jasonmelgoza <jason@rightscale.com>
2015-05-21 14:26:49 -0700

    make it so

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name skrillexApp
+ * @description
+ * # skrillexApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('skrillexApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skrillexApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the skrillexApp
+ */
+angular.module('skrillexApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
