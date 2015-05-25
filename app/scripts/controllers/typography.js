d38646e0cff6382cc56f0df8056c21ae9a4d5317
Jason Melgoza <jasonmelgoza@gmail.com>
2015-05-25 15:55:36 -0700

    added type section

---
+++ app/scripts/controllers/typography.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skrillexApp.controller:TypographyCtrl
+ * @description
+ * # TypographyCtrl
+ * Controller of the skrillexApp
+ */
+angular.module('skrillexApp')
+  .controller('TypographyCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
