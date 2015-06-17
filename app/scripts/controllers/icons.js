a930b8e95d090efce664bac2f91692309edf687e
jasonmelgoza <jason@rightscale.com>
2015-06-17 11:07:06 -0700

    added more icons

--- app/scripts/controllers/icons.js
+++ app/scripts/controllers/icons.js
@@ -14,21 +14,77 @@ angular.module('designkitApp')
         id: 1,
         item: {
           name: 'Trash',
-          src: '',
+          src: 'images/icon/icon-trash.svg',
         }
       },
       {
         id: 2,
         item: {
           name: 'Trigger',
-          src: '',
+          src: 'images/icon/icon-switch-trigger.svg',
         }
       },
       {
         id: 3,
         item: {
           name: 'External Blue',
-          src: '/images/icon/icon-external-blue.svg',
+          src: 'images/icon/icon-external-blue.svg',
+        }
+      },
+      {
+        id: 4,
+        item: {
+          name: 'Pen',
+          src: 'images/icon/icon-pen.svg',
+        }
+      },
+      {
+        id: 5,
+        item: {
+          name: 'Logout',
+          src: 'images/icon/icon-user-logout.svg',
+        }
+      },
+      {
+        id: 6,
+        item: {
+          name: 'Share',
+          src: 'images/icon/icon-share.svg',
+        }
+      },
+      {
+        id: 7,
+        item: {
+          name: 'Search',
+          src: 'images/icon/icon-search.svg',
+        }
+      },
+      {
+        id: 8,
+        item: {
+          name: 'Refresh',
+          src: 'images/icon/icon-refresh.svg',
+        }
+      },
+      {
+        id: 9,
+        item: {
+          name: 'Markdown',
+          src: 'images/icon/icon-markdown.svg',
+        }
+      },
+      {
+        id: 10,
+        item: {
+          name: 'Link',
+          src: 'images/icon/icon-link.svg',
+        }
+      },
+      {
+        id: 11,
+        item: {
+          name: 'Info',
+          src: 'images/icon/icon-info.svg',
         }
       }
     ];
