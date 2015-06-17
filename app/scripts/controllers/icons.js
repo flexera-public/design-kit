f1521c8cfe2d7990df99dc89acb1f9c1d405e8a7
allancorbett <allan.corbett@rightscale.com>
2015-06-17 11:33:18 -0700

    Updated icons

--- app/scripts/controllers/icons.js
+++ app/scripts/controllers/icons.js
@@ -13,71 +13,71 @@ angular.module('designkitApp')
       {
         id: 1,
         item: {
-          name: 'Trash',
-          src: 'images/icon/icon-trash.svg',
+          name: 'Arrow',
+          src: 'images/icon/icon-arrow.svg',
         }
       },
       {
         id: 2,
         item: {
-          name: 'Trigger',
-          src: 'images/icon/icon-switch-trigger.svg',
+          name: 'Calendar',
+          src: 'images/icon/icon-calendar.svg',
         }
       },
       {
         id: 3,
+        item: {
+          name: 'Caret',
+          src: 'images/icon/icon-caret.svg',
+        }
+      },
+      {
+        id: 4,
+        item: {
+          name: 'Close Blue',
+          src: 'images/icon/icon-close-blue.svg',
+        }
+      },
+      {
+        id: 5,
+        item: {
+          name: 'Close',
+          src: 'images/icon/icon-close.svg',
+        }
+      },
+      {
+        id: 6,
+        item: {
+          name: 'Download',
+          src: 'images/icon/icon-download.svg',
+        }
+      },
+      {
+        id: 7,
+        item: {
+          name: 'Expand',
+          src: 'images/icon/icon-expand.svg',
+        }
+      },
+      {
+        id: 8,
+        item: {
+          name: 'Export',
+          src: 'images/icon/icon-export.svg',
+        }
+      },
+      {
+        id: 9,
         item: {
           name: 'External Blue',
           src: 'images/icon/icon-external-blue.svg',
         }
       },
       {
-        id: 4,
-        item: {
-          name: 'Pen',
-          src: 'images/icon/icon-pen.svg',
-        }
-      },
-      {
-        id: 5,
-        item: {
-          name: 'Logout',
-          src: 'images/icon/icon-user-logout.svg',
-        }
-      },
-      {
-        id: 6,
-        item: {
-          name: 'Share',
-          src: 'images/icon/icon-share.svg',
-        }
-      },
-      {
-        id: 7,
-        item: {
-          name: 'Search',
-          src: 'images/icon/icon-search.svg',
-        }
-      },
-      {
-        id: 8,
-        item: {
-          name: 'Refresh',
-          src: 'images/icon/icon-refresh.svg',
-        }
-      },
-      {
-        id: 9,
-        item: {
-          name: 'Markdown',
-          src: 'images/icon/icon-markdown.svg',
-        }
-      },
-      {
         id: 10,
         item: {
-          name: 'Link',
-          src: 'images/icon/icon-link.svg',
+          name: 'External White',
+          src: 'images/icon/icon-external-white.svg',
         }
       },
       {
@@ -85,6 +85,83 @@ angular.module('designkitApp')
         item: {
           name: 'Info',
           src: 'images/icon/icon-info.svg',
+        }
+      },
+      {
+        id: 12,
+        item: {
+          name: 'Link',
+          src: 'images/icon/icon-link.svg',
+        }
+      },
+      {
+        id: 13,
+        item: {
+          name: 'Markdown',
+          src: 'images/icon/icon-markdown.svg',
+        }
+      },
+      {
+        id: 14,
+        item: {
+          name: 'Pen',
+          src: 'images/icon/icon-pen.svg',
+        }
+      },
+      {
+        id: 15,
+        item: {
+          name: 'Plus Open',
+          src: 'images/icon/icon-plus-open.svg',
+        }
+      },
+      {
+        id: 16,
+        item: {
+          name: 'Product Trigger',
+          src: 'images/icon/icon-product-trigger.svg',
+        }
+      },
+      {
+        id: 17,
+        item: {
+          name: 'Refresh',
+          src: 'images/icon/icon-refresh.svg',
+        }
+      },
+      {
+        id: 18,
+        item: {
+          name: 'Search',
+          src: 'images/icon/icon-search.svg',
+        }
+      },
+      {
+        id: 19,
+        item: {
+          name: 'Share',
+          src: 'images/icon/icon-share.svg',
+        }
+      },
+      {
+        id: 21,
+        item: {
+          name: 'Timepicker Colon',
+          src: 'images/icon/icon-timepicker-colon.svg',
+        }
+      },
+      {
+        id: 22,
+        item: {
+          name: 'Trash',
+          src: 'images/icon/icon-trash.svg',
+        }
+      },
+      {
+        id: 23,
+        item: {
+          name: 'User Logout',
+          src: 'images/icon/icon-user-logout.svg',
         }
       }
     ];
