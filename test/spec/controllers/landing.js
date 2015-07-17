1ed1321759eddde044388d2ad398eaf0a1ea49a2
jasonmelgoza <jason@rightscale.com>
2015-07-17 14:10:12 -0700

    new build

--- test/spec/controllers/landing.js
+++ test/spec/controllers/landing.js
@@ -16,7 +16,7 @@ describe('Controller: LandingCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of cards to the scope', function () {
+    expect(scope.cards.length).toBe(7);
   });
 });
