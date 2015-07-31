a7ec2ec9c65d8b438836a823afa16ec20f9e7e08
Jakub Hampl <honitom@seznam.cz>
2015-07-31 12:10:36 +0200

    Add indeterminate checkboxes

--- app/scripts/directives/select_all.js
+++ app/scripts/directives/select_all.js
@@ -3,48 +3,60 @@
 /**
  * @ngdoc selectAll
  * @name designkitApp.directive:selectAll
- * @restrict A
- * @param {Array} selectAll A collection of rows that this manages.
- * @param {String} selectionAttribute The name of the attribute in the row that holds selection state.
+ * @restrict E
+ * @param {Array} collection A collection of rows that this manages.
+ * @param {String} attribute The name of the attribute in the row that holds selection state.
  * @description Manages a selectAll checkbox.
  */
 angular.module('designkitApp').directive('selectAll', function() {
   return {
-    restrict: 'A',
+    restrict: 'E',
+    template: '<div class="checkbox_wrap">' +
+      '<input type="checkbox" id="checkbox-all-{{id}}" />' +
+      '<label for="checkbox-all-{{id}}" class="checkbox_label"><span class="sr-only">{{label}}</span></label>' +
+     '</div>',
     scope: {
-      selectAll: '=',
-      selectionAttribute: '@'
+      collection: '=',
+      attribute: '@'
     },
     link: function(scope, element) {
-      element.on('change', function() {
+      scope.id = Math.floor(10000 * Math.random());
+      var checkbox = element.find('input');
+      checkbox.on('change', function() {
         scope.$apply(function() {
-          if (element.prop('checked')) {
-            scope.selectAll.forEach(function(el) {
-              el[scope.selectionAttribute] = true;
+          if (checkbox.prop('checked')) {
+            scope.collection.forEach(function(el) {
+              el[scope.attribute] = true;
             });
           } else {
-            scope.selectAll.forEach(function(el) {
-              el[scope.selectionAttribute] = false;
+            scope.collection.forEach(function(el) {
+              el[scope.attribute] = false;
             });
           }
         });
       });
-      scope.$watch('selectAll', function(collection) {
+      scope.$watch('collection', function(collection) {
         var all = true,
             some = false;
         collection.forEach(function(element) {
-          if (element[scope.selectionAttribute]) {
+          if (element[scope.attribute]) {
             some = true;
           } else {
             all = false;
           }
         });
         if (all) {
-          element.prop('checked', true).prop('indeterminate', false);
+          element.find('div').removeClass('checkbox_indeterminate');
+          scope.label = 'Select none';
+          checkbox.prop('checked', true).prop('indeterminate', false);
         } else if (some) {
-          element.prop('checked', false).prop('indeterminate', true);
+          element.find('div').addClass('checkbox_indeterminate');
+          scope.label = 'Select all';
+          checkbox.prop('checked', false).prop('indeterminate', true);
         } else {
-          element.prop('checked', false).prop('indeterminate', false);
+          element.find('div').removeClass('checkbox_indeterminate');
+          scope.label = 'Select all';
+          checkbox.prop('checked', false).prop('indeterminate', false);
         }
       }, true);
     }
