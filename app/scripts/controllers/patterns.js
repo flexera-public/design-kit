c3d1c5a90d5c0be41e411ebb829f51bb77ca7cef
jasonmelgoza <jason@rightscale.com>
2015-08-07 21:54:59 -0700

    dk - added pattern layout

--- app/scripts/controllers/patterns.js
+++ app/scripts/controllers/patterns.js
@@ -8,10 +8,20 @@
  * Controller of the designkitApp
  */
 angular.module('designkitApp')
-  .controller('PatternsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PatternsCtrl', function ($scope, $rootScope) {
+    $scope.patterns = [{
+      name: 'App structure',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum consequuntur, quis cupiditate, corrupti deleniti labore eos commodi et quae. Reprehenderit obcaecati possimus vero quam illum! Quae maiores earum expedita unde.',
+      example: '',
+      reference: ''
+    }, {
+      name: 'Header',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum consequuntur, quis cupiditate, corrupti deleniti labore eos commodi et quae. Reprehenderit obcaecati possimus vero quam illum! Quae maiores earum expedita unde.',
+      example: '',
+      reference: ''
+    }];
+
+    $scope.sectionClass = 'section-patterns';
+    $rootScope.bodyClass = 'layout-patterns';
+
   });
