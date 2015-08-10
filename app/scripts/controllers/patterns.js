7a71316a4317f96111ebf6f92b18fff789205405
jasonmelgoza <jason@rightscale.com>
2015-08-10 08:40:55 -0700

    dk - added patterns to nav

--- app/scripts/controllers/patterns.js
+++ app/scripts/controllers/patterns.js
@@ -19,6 +19,11 @@ angular.module('designkitApp')
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum consequuntur, quis cupiditate, corrupti deleniti labore eos commodi et quae. Reprehenderit obcaecati possimus vero quam illum! Quae maiores earum expedita unde.',
       example: '',
       reference: ''
+    }, {
+      name: 'Navs',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum consequuntur, quis cupiditate, corrupti deleniti labore eos commodi et quae. Reprehenderit obcaecati possimus vero quam illum! Quae maiores earum expedita unde.',
+      example: '',
+      reference: ''
     }];
 
     $scope.sectionClass = 'section-patterns';
