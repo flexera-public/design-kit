0479afc0a0da54904a873ce9b372206d34d28352
Josh Morse <joshua.morse@rightscale.com>
2015-08-25 03:23:59 +0100

    Updated copy for sprites.

--- app/scripts/controllers/sprites.js
+++ app/scripts/controllers/sprites.js
@@ -19,79 +19,85 @@ angular.module('designkitApp')
           {
             name: 'Delete',
             src: 'images/sprites/action/delete.svg',
-            description: 'Delete an item or component'
+            description: 'Delete an item or component.'
           },
           {
             name: 'Lock',
             src: 'images/sprites/action/padlock-closed.svg',
-            description: 'Indicates the RightScale component (e.g. Deployment) is locked.  The Lock icon is a toggle.  Clicking it will set status to "unlock", making it editable'
+            description: 'Indicates the RightScale component (e.g. Deployment) is locked. Clicking it will set status to unlocked, allowing the component and its children to be terminated and deleted.'
           },
           {
             name: 'Unlock',
             src: 'images/sprites/action/padlock-open.svg',
-            description: 'Indicates the RightScale component (e.g. Deployment) is unlocked.  Clicking it will set status to "lock", making it uneditable'
+            description: 'Indicates the RightScale component (e.g. Deployment) is unlocked. Clicking it will set status to locked, preventing the component and its children from being terminated or deleted.'
           },
         ]
       },
       {
         group: {
-          name: 'Informational',
-          description: 'Standard informational icons are defined below.  Most icons are static, but a few do change the display in the Dashboard, yet take not action with respect to any cloud asssets.'
+          name: 'Status',
+          description: 'Standard status icons are defined below.  Most icons are static, but a few do change the display in the Dashboard, yet do not take action with respect to any cloud asssets.'
         },
         sprites: [
           {
-            name: 'Circle Green',
-            src: 'images/sprites/info/circle-green.svg'
+            name: 'Alerts Untriggered / Active Instances',
+            src: 'images/sprites/info/circle-green.svg',
+            description: 'Indicates that no alerts are currently triggered. Also indicates the number of active instances in an array.'
+          },
+          {
+            name: 'Alerts Triggered',
+            src: 'images/sprites/info/circle-red.svg',
+            description: 'Indicates that one or more alerts are currently triggered.'
           },
           {
             name: 'Circle Orange',
-            src: 'images/sprites/info/circle-orange.svg'
-          },
-          {
-            name: 'Invalid',
-            src: 'images/sprites/info/circle-red.svg',
-            description: 'Invalid'
+            src: 'images/sprites/info/circle-orange.svg',
+            description: 'Currently unused. May be needed for additional alert status.'
           },
           {
             name: 'Circle Blue',
-            src: 'images/sprites/info/circle-blue.svg'
+            src: 'images/sprites/info/circle-blue.svg',
+            description: 'Currently unused. May be needed for additional alert status.'  
           },
           {
             name: 'Square Green',
-            src: 'images/sprites/info/square-green.svg'
+            src: 'images/sprites/info/square-green.svg',
+            description: 'Currently unused. May be needed for additional alert status.'  
           },
           {
             name: 'Square Orange',
-            src: 'images/sprites/info/square-orange.svg'
+            src: 'images/sprites/info/square-orange.svg',
+            description: 'Indicates that a server or instance is in the stopped or provisioned state.'  
           },
           {
-            name: 'Stopped',
+            name: 'Offline',
             src: 'images/sprites/info/square-red.svg',
-            description: 'A Server whose state is "stopped".  A true "stopped Server" is currently an AWS EC2 centric term.  The same icon is used for "offline" Servers.'
+            description: 'A Server whose state is offline, or a server array which has no active instances.'
           },
           {
             name: 'Booting',
             src: 'images/sprites/info/arrow-up-green.svg',
-            description: 'An instance has been launched, and its current state is "booting"'
+            description: 'An instance has been launched, and its current state is booting.'
           },
           {
             name: 'Pending',
             src: 'images/sprites/info/arrow-up-orange.svg',
-            description: 'An instance has been launched, and its current state is "pending"'
+            description: 'An instance has been launched, and its current state is pending.'
           },
           {
-            name: 'Decommissioning ',
+            name: 'Decommissioning',
             src: 'images/sprites/info/arrow-down-green.svg',
-            description: 'A Server is in the decommissioning process'
+            description: 'A Server is in the decommissioning state.'
           },
           {
             name: 'Arrow Down',
-            src: 'images/sprites/info/arrow-down-orange.svg'
+            src: 'images/sprites/info/arrow-down-orange.svg',
+            description: 'Currently unused. May be needed for additional instance status.' 
           },
           {
             name: 'Warning',
             src: 'images/sprites/info/warning.svg',
-            description: 'Hover over the icon for a specific message. For example, a HEAD revision RightScript warning message'
+            description: 'Indicates that a component is in an error or warning state. Hover over the icon to bring up a tooltip with additional details.'
           },
         ]
       },
@@ -106,15 +112,15 @@ angular.module('designkitApp')
             src: 'images/sprites/bookmarks/server_bm.svg'
           },
           {
-            name: 'Deployments',
+            name: 'Deployment',
             src: 'images/sprites/bookmarks/deployments_bm.svg'
           },
           {
-            name: 'Instances',
+            name: 'Instance',
             src: 'images/sprites/bookmarks/instances_bm.svg'
           },
           {
-            name: 'Server Templates',
+            name: 'Server Template',
             src: 'images/sprites/bookmarks/server_templates_bm.svg'
           },
           {
