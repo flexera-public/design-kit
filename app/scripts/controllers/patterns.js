112531ee240834242e0c0d70e0b6eb7d82cd6a75
jasonmelgoza <jason@rightscale.com>
2015-09-04 15:04:08 -0700

    dk - updated pattern lib

--- app/scripts/controllers/patterns.js
+++ app/scripts/controllers/patterns.js
@@ -9,12 +9,21 @@
  */
 angular.module('designkitApp')
   .controller('PatternsCtrl', function ($scope, $rootScope) {
-    $scope.patterns = [{
-      name: 'Title area',
-      description: 'Contains the page/section title, breadcrumbs and secondary navigation. The titlebar can also contain some primary such as "Create".',
-      example: 'images/ignore/pattern-titlearea.png',
-      reference: 'Self-Service, Cloud Analytics, Cloud Management'
-    }];
+    $scope.patterns = [
+      {
+        name: 'Title area',
+        description: 'A common pattern used to group the section title and any secondary navigation - the title area contains the page/section title, secondary navigation and breadcrumbs. It also can contain primary buttons such as "Create" or "New."',
+        example: 'images/ignore/pattern-titlearea.png',
+        reference: 'Self-Service, Cloud Analytics, Cloud Management',
+        border: 'border-light'
+      },
+      {
+        name: 'Activity stream',
+        description: 'Activity streams are used to summerize time-ordered activity in a context and link to objects they reference.',
+        example: 'images/ignore/pattern-activity.png',
+        reference: 'Self-Service'
+      }
+    ];
 
     $scope.sectionClass = 'section-patterns';
     $rootScope.bodyClass = 'layout-patterns';
