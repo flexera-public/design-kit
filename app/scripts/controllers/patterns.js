bb922bf9c25b16e29470771174be3498286e9088
jasonmelgoza <jason@rightscale.com>
2015-09-14 11:52:18 -0700

    dk - added header pattern

--- app/scripts/controllers/patterns.js
+++ app/scripts/controllers/patterns.js
@@ -11,17 +11,33 @@ angular.module('designkitApp')
   .controller('PatternsCtrl', function ($scope, $rootScope) {
     $scope.patterns = [
       {
-        name: 'Title area',
-        description: 'A common pattern used to group the section title and any secondary navigation - the title area contains the page/section title, secondary navigation and breadcrumbs. It also can contain primary buttons such as "Create" or "New."',
-        example: 'images/ignore/pattern-titlearea.png',
-        reference: 'Self-Service, Cloud Analytics, Cloud Management',
-        border: 'border-light'
+        id: 1,
+        group: {
+          name: 'Header',
+          description: 'One of our dominate UI patterns, the product header includes the main and product logos and our product dropdown trigger.',
+          example: 'images/ignore/pattern-header-1.png',
+          reference: 'Self-Service, Cloud Analytics, Cloud Management',
+          border: 'border-light'
+        }
       },
       {
-        name: 'Activity stream',
-        description: 'Activity streams are used to summerize time-ordered activity in a context and link to objects they reference.',
-        example: 'images/ignore/pattern-activity.png',
-        reference: 'Self-Service'
+        id: 2,
+        group: {
+          name: 'Title area',
+          description: 'A common pattern used to group the section title and any secondary navigation - the title area contains the page/section title, secondary navigation and breadcrumbs. It also can contain primary buttons such as "Create" or "New."',
+          example: 'images/ignore/pattern-titlearea.png',
+          reference: 'Self-Service, Cloud Analytics, Cloud Management',
+          border: 'border-light'
+        }
+      },
+      {
+        id: 3,
+        group: {
+          name: 'Activity stream',
+          description: 'Activity streams are used to summerize time-ordered activity in a context and link to objects they reference.',
+          example: 'images/ignore/pattern-activity.png',
+          reference: 'Self-Service'
+        }
       }
     ];
 
