1541c20707f1a467407bce34ea1e8b1c8196a1b8
jasonmelgoza <jason@rightscale.com>
2016-01-20 23:03:36 -0800

    dk - added docs base

---
+++ docs/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name docsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the docsApp
+ */
+angular.module('docsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ docs/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name docsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the docsApp
+ */
+angular.module('docsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
