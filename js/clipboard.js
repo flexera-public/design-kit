65d19857f58017c1614a31ee9c973dbb3c4be0e3
jasonmelgoza <jason@rightscale.com>
2016-09-16 17:33:22 -0700

    #127 Added clipboard js and implemented in colors

---
+++ js/clipboard.js
@@ -0,0 +1,9 @@
+var clipboard = new Clipboard('.clipboard');
+
+// clipboard.on('success', function(e) {
+//     console.info('Action:', e.action);
+//     console.info('Text:', e.text);
+//     console.info('Trigger:', e.trigger);
+//
+//     e.clearSelection();
+// });
