3ca1fb52b68199e705d13fe96f46bbce03cbd292
Josh <joshua.morse@rightscale.com>
2017-07-19 17:54:07 -0500

    Ux 540 added pattern annotations (#153)
    * Initial annotations with click overlay
    
    * Added files
    
    * 3 patterns done. Still need some CSS updates.
    
    * Added product selector
    
    * Consolidated files.

---
+++ js/patterns.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($){
+	//open interest point description
+	$('.cd-single-point').children('a').on('click', function(){
+		var selectedPoint = $(this).parent('li');
+		if( selectedPoint.hasClass('is-open') ) {
+			selectedPoint.removeClass('is-open').addClass('visited');
+		} else {
+			selectedPoint.addClass('is-open').siblings('.cd-single-point.is-open').removeClass('is-open').addClass('visited');
+		}
+	});
+	//close interest point description
+	$('.cd-close-info').on('click', function(event){
+		event.preventDefault();
+		$(this).parents('.cd-single-point').eq(0).removeClass('is-open').addClass('visited');
+	});
+});
+
+function toggleAnnotations() {
+  if ($(".cd-single-point:visible").length > 0)
+    $(".cd-single-point").hide();
+  else
+    $(".cd-single-point").show();
+}
